9824cb4e72b1f972dfa2d2502ae783f699105923
Souverain joseph <158466435+Souverain08@users.noreply.github.com>
2024-10-19 18:09:11 +0200

    custom Login and sin up

---
+++ screens/ForgotPassword.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const ForgotPassword = () => {
+  return (
+    <View>
+      <Text>Forgot password</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ForgotPassword;
--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,175 +1,279 @@
-import React, { useState } from 'react';
-import {View,SafeAreaView, StyleSheet, Text, Image,TextInput, TouchableOpacity, Pressable} from 'react-native';
-import { colors } from '../constant/Constant';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { MaterialIcons } from '@expo/vector-icons';
-import Button from '../component/Button';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as ImagePicker from 'expo-image-picker'
+import React, { useState } from "react";
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
+import { colors } from "../constant/Constant";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
+import Button from "../component/Button";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as ImagePicker from "expo-image-picker";
+import { TextInput } from "react-native-paper";
+// import { GoogleSvg, FacebookSvg } from "../iconSvg/icon";
 
-const Login = ({navigation}) => {
-    const [isSinIn, setIsSinIn] = useState(false)
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [userImg, setUserImg] = useState('')
-    const [isChecked, setIsChecked] = useState(false)
+const Login = ({ navigation }) => {
+  const [isSinIn, setIsSinIn] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userImg, setUserImg] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
 
-    const handleNavigation = async () => {
-            
-            if(!isSinIn && name.length > 0 && email.length > 0 && password.length > 0) {
-                const user = {
-                    name:name,
-                    email:email,
-                    password:password,
-                    userImg: userImg
-                }
-                await AsyncStorage.setItem('userProfil', JSON.stringify(user))
-                navigation.navigate('Welecom')
-            } else if(isSinIn && email.length > 0 && password.length > 0){
-                const user = {
-                    email:email,
-                    password:password
-                }
-                await AsyncStorage.setItem('userProfil', JSON.stringify(user))
-                navigation.navigate('Welecom')
-            } else {
-                alert('Veiller remplir tout les champs')
-            }
-
-    }
-
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.All,
-          allowsEditing: true,
-          aspect: [4, 3],
-          quality: 1,
-        });
-    
-    
-        if (!result.canceled) {
-            setUserImg(result.assets[0].uri);
-            setIsChecked(true)
-        }
+  const handleNavigation = async () => {
+    if (
+      !isSinIn &&
+      name.length > 0 &&
+      email.length > 0 &&
+      password.length > 0
+    ) {
+      const user = {
+        name: name,
+        email: email,
+        password: password,
+        userImg: userImg,
       };
-    return (
-        <SafeAreaView style={styles.container}>
-            {!isSinIn ?
-                <View style={styles.containerImg}>
-                <Image
-                    source = {!isChecked ? require('../img/profil.png') : {uri:userImg}}
-                    style = {styles.userImg}
-                />
-                <TouchableOpacity 
-                    onPress={pickImage}
-                    style={styles.iconImg}
-                >
-                    <MaterialIcons style={styles.icon} name="add-photo-alternate" />
-                </TouchableOpacity>
+      await AsyncStorage.setItem("userProfil", JSON.stringify(user));
+      navigation.navigate("Welecom");
+    } else if (isSinIn && email.length > 0 && password.length > 0) {
+      const user = {
+        email: email,
+        password: password,
+      };
+      await AsyncStorage.setItem("userProfil", JSON.stringify(user));
+      navigation.navigate("Welecom");
+    } else {
+      alert("Veiller remplir tout les champs");
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setUserImg(result.assets[0].uri);
+      setIsChecked(true);
+    }
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("../img/bgWelecom.jpg")}
+        resizeMode="cover"
+        style={styles.containerTwo}
+      >
+        <View style={styles.overlay} />
+        <ScrollView
+          contentContainerStyle={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {!isSinIn ? (
+            <View style={styles.containerImg}>
+              <Image
+                source={
+                  !isChecked ? require("../img/profil.png") : { uri: userImg }
+                }
+                style={styles.userImg}
+              />
+              <TouchableOpacity onPress={pickImage} style={styles.iconImg}>
+                <MaterialIcons style={styles.icon} name="add-photo-alternate" />
+              </TouchableOpacity>
             </View>
-            : 
-            <Text style={styles.titleSin}>Sign Up</Text>
-            }
-                
-                <View style={styles.containerInput}>
-                    {!isSinIn &&
-                        <TextInput
-                            placeholder='Name'
-                            style={styles.input}
-                            onChangeText={text => setName(text)}
-                        />
-                    }
-                    <TextInput
-                        placeholder='Email'
-                        keyboardType='email-address'
-                        style={styles.input}
-                        onChangeText={text => setEmail(text)}
-                    />
-                    <TextInput
-                        placeholder='PassWord'
-                        secureTextEntry
-                        style={styles.input}
-                        onChangeText={text => setPassword(text)}
-                    />
-                    <Button
-                        style={styles.Button}
-                        handleNavigation={handleNavigation}
-                        iconSize={24}
-                        iconColor={colors.WHITE}
-                        btnTitle={"Sign Up"}
-                    />
-                    <TouchableOpacity
-                        onPress={() => setIsSinIn(changed=>!changed)}
-                    >
-                        <Text style={styles.isSinIn}> 
-                            {isSinIn? "to sign up" : "to login"}
-                        </Text>
-                    </TouchableOpacity>
-           </View>
-        </SafeAreaView>
-    );
-}
+          ) : (
+            <Text style={styles.titleSin}>Login</Text>
+          )}
+
+          <View style={styles.containerInput}>
+            {!isSinIn && (
+              <TextInput
+                mode="outlined"
+                label="Name"
+                placeholder="Your name"
+                right={<TextInput.Affix />}
+                theme={{
+                  colors: {
+                    primary: colors.GREEN, // Outline and label color when focused
+                    background: "rgba(14, 56, 84, 0.5)",
+                  },
+                }}
+              />
+            )}
+            <TextInput
+              mode="outlined"
+              label="Email"
+              placeholder="Your email"
+              right={<TextInput.Affix />}
+              style={{ marginVertical: 10 }}
+              theme={{
+                colors: {
+                  primary: colors.GREEN, // Outline and label color when focused
+                  background: "rgba(14, 56, 84, 0.5)",
+                },
+              }}
+            />
+            <TextInput
+              mode="outlined"
+              label="Password"
+              placeholder="Your password"
+              right={<TextInput.Affix />}
+              theme={{
+                colors: {
+                  primary: colors.GREEN, // Outline and label color when focused
+                  background: "rgba(14, 56, 84, 0.5)",
+                },
+              }}
+            />
+            <Button
+              style={styles.Button}
+              handleNavigation={handleNavigation}
+              iconSize={24}
+              iconColor={colors.WHITE}
+              btnTitle={isSinIn ? "Login" : "sign up"}
+            />
+            <Text style={styles.isSinInOne}>Or</Text>
+            <View style={styles.iconContainer}>
+              <View style={styles.iconCheeld}>
+                <MaterialIcons name="facebook" size={38} color="white" />
+              </View>
+              <View style={styles.iconCheeld}>
+                <AntDesign name="google" size={38} color="white" />
+              </View>
+            </View>
+            <View style={styles.containerTitle}>
+              <Text style={styles.isSinIn}>
+                {isSinIn
+                  ? "Don't have a account ? "
+                  : "Already have an account ?"}
+              </Text>
+              <TouchableOpacity
+                onPress={() => setIsSinIn((changed) => !changed)}
+              >
+                <Text style={styles.isSinInOne}>
+                  {isSinIn ? "Sign up" : "Login"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+            {isSinIn && (
+              <TouchableOpacity>
+                <Text style={{ ...styles.isSinInOne, paddingVertical: 10 }}>
+                  Forgot your password ?
+                </Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </ScrollView>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: colors.BACK,
-        
-    },
-   
-    userImg: {
-        width: wp('20%'),
-        height: hp('10%'),
-        borderRadius: wp('15%'),
-        marginTop: hp('1.5%')
-    },
-    containerImg: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    iconImg: {
-        alignSelf:'flex-end',
-        paddingHorizontal: wp('2%')
-    },
-    icon: {
-        fontSize:wp('7%'),
-        color: colors.GREEN
-    },
-    input: {
-        backgroundColor: colors.GREEN,
-        borderRadius: wp('4%'),
-        paddingHorizontal: wp('5%') ,
-        paddingVertical: hp('1%'),
-        fontSize: wp('3%'),
-        marginVertical: hp('2%')
-    },
-    containerInput: {
-        width: wp('100%'),
-        paddingHorizontal: wp('10%'),
-        paddingVertical: hp('3%')
-    },
-    Button: {
-        backgroundColor: colors.ORANGE,
-        borderRadius: wp('4%'),
-        paddingHorizontal: wp('5%') ,
-        paddingVertical: hp('1.4%'),
-        alignItems: 'center',
-        marginVertical: hp('4')
-    },
-    isSinIn: {
-        textAlign: 'center',
-        color: colors.GREEN,
-        fontSize: wp('4%')
-    },
-    titleSin:{
-        fontSize: hp('3%'),
-        textAlign: 'center',
-        color: colors.WHITE,
-        fontWeight: 'bold'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: colors.BACK,
+  },
+  containerTwo: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject, // Fills the entire ImageBackground
+    backgroundColor: "rgba(0, 0, 0, 0.7)", // Black with 50% opacity
+  },
+
+  userImg: {
+    width: wp("20%"),
+    height: hp("10%"),
+    borderRadius: wp("15%"),
+    marginTop: hp("1.5%"),
+  },
+  containerImg: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  iconImg: {
+    alignSelf: "flex-end",
+    paddingHorizontal: wp("2%"),
+  },
+  icon: {
+    fontSize: wp("7%"),
+    color: colors.GREEN,
+  },
+  input: {
+    backgroundColor: colors.GREEN,
+    borderRadius: wp("4%"),
+    paddingHorizontal: wp("5%"),
+    paddingVertical: hp("1%"),
+    fontSize: wp("3%"),
+    marginVertical: hp("2%"),
+  },
+  containerInput: {
+    width: wp("100%"),
+    paddingHorizontal: wp("10%"),
+    paddingVertical: hp("3%"),
+  },
+  Button: {
+    backgroundColor: colors.ORANGE,
+    borderRadius: wp("1.5%"),
+    paddingHorizontal: wp("5%"),
+    paddingVertical: hp("1.4%"),
+    alignItems: "center",
+    marginVertical: hp("2"),
+  },
+  isSinInOne: {
+    textAlign: "center",
+    color: colors.ORANGE,
+    fontSize: wp("4%"),
+  },
+  isSinIn: {
+    textAlign: "center",
+    color: colors.GREEN,
+    fontSize: wp("4%"),
+  },
+  titleSin: {
+    fontSize: hp("3%"),
+    textAlign: "center",
+    color: colors.WHITE,
+    fontWeight: "bold",
+  },
+  containerTitle: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 4,
+  },
+  iconCheeld: {
+    backgroundColor: "rgba(14, 56, 84, 0.5)",
+    padding: 10,
+    borderRadius: wp("1.5%"),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+    paddingVertical: 10,
+  },
+});
 
 export default Login;
