e72be61d1ff2afbf33dbd6e64f9e69cbc9389de8
Souverain joseph <158466435+Souverain08@users.noreply.github.com>
2024-10-22 23:50:47 +0200

    Custom welecom

--- screens/Welecom.jsx
+++ screens/Welecom.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Image, ActivityIndicator } from "react-native";
+import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 import { colors } from "../constant/Constant";
 import Button from "../component/Button";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
 const Welecom = ({ navigation }) => {
   const [isLod, setIsLod] = useState(false);
@@ -15,13 +16,29 @@ const Welecom = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <Image source={require("../img/BingoImg.png")} style={styles.imgBingo} />
-      <View style={styles.containerSheeld}>
-        <Text style={styles.titleWelcom}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
-          expedita facere. Aspernatur laboriosam nesciunt, impedit minus
-        </Text>
+    <ImageBackground
+      source={require("../img/welecom.jpg")}
+      resizeMode="cover"
+      style={styles.container}
+    >
+      <View
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          backgroundColor: "#0E3854",
+          opacity: 0.8,
+        }}
+      />
+      <Animated.Image
+        entering={FadeInDown.delay(400).springify()}
+        resizeMode="cover"
+        source={require("../img/BingoImg.png")}
+        style={styles.imgBingo}
+      />
+      <Animated.View
+        entering={FadeInDown.delay(500).springify()}
+        style={styles.containerSheeld}
+      >
+        <Text style={styles.titleWelcom}>the best recipe in the wold</Text>
         <Button
           style={styles.Button}
           handleNavigation={handleNavigation}
@@ -29,8 +46,8 @@ const Welecom = ({ navigation }) => {
           iconColor={colors.WHITE}
           btnTitle={"Started Explore"}
         />
-      </View>
-    </View>
+      </Animated.View>
+    </ImageBackground>
   );
 };
 
@@ -38,30 +55,31 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.BACK,
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-end",
     alignItems: "center",
+    paddingBottom: hp("3%"),
   },
   containerSheeld: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 50,
+    paddingHorizontal: wp(5),
   },
   imgBingo: {
-    width: 400,
-    height: 300,
+    width: 300,
+    height: 165,
   },
   titleWelcom: {
     color: colors.WHITE,
-    fontWeight: "bold",
-    fontSize: 15,
-    marginVertical: 50,
+    fontWeight: "600",
+    fontSize: hp(1.4),
+    // marginVertical: ,
   },
   Button: {
+    width: wp("90%"),
     backgroundColor: colors.ORANGE,
-    borderRadius: wp("1.5%"),
-    paddingHorizontal: wp("5%"),
+    borderRadius: wp("3%"),
     paddingVertical: hp("1.4%"),
     alignItems: "center",
     marginVertical: hp("2"),
